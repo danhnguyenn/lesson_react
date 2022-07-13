ec0c27088bb36299024f7f1dcab4eb7ddeddeea1
Danh Nguyen <danhntc.fe@gmail.com>
2022-07-13 16:52:12 +0700

    Edit num

--- 03_Lifecycle/src/components/counter/Num.js
+++ 03_Lifecycle/src/components/counter/Num.js
@@ -8,8 +8,8 @@ class Num extends React.Component {
         const { value, onIncrease, onDescrease } = this.props;
         return <>
             <h3 style={{ fontWeight: 'bold' }}>Value: {this.props.value}</h3>
-            <button class="btn btn-primary" onClick={onIncrease}>Increase</button>
-            <button class="btn btn-secondary" onClick={onDescrease}>Decrease</button>
+            <button className="btn btn-primary" onClick={onIncrease}>Increase</button>
+            <button className="btn btn-secondary" onClick={onDescrease}>Decrease</button>
         </>
     }
 }
